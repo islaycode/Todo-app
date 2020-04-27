691898e21b4ef4e4fe2fd3fb0d5a2d2e824289f2
Nikhil <nikzdev@gmail.com>
2020-04-27 18:35:29 +0530

    Updates

---
+++ precache-manifest.e241effdc9057d9a389d5d4be09704e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed02c0902edb59e4452c7a90cdbce95a",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "71c1eefd8aa3a07ea881",
+    "url": "/todoapp/static/css/main.1101daf2.chunk.css"
+  },
+  {
+    "revision": "273410781497198085b2",
+    "url": "/todoapp/static/js/2.85c3c5d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.85c3c5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c1eefd8aa3a07ea881",
+    "url": "/todoapp/static/js/main.fe27a6ff.chunk.js"
+  },
+  {
+    "revision": "5db51c626a1c737e7f36",
+    "url": "/todoapp/static/js/runtime-main.b28f0bac.js"
+  }
+]);
